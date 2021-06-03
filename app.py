86646fc25c5975d6c8c57ee4d936c08577e6d39c
Lucy Weatherley <lucyweath@msn.com>
2021-06-03 13:39:58 +0100

    Replace venues page data

--- app.py
+++ app.py
@@ -98,28 +98,17 @@ def index():
 def venues():
   # TODO: replace with real venues data.
   #       num_shows should be aggregated based on number of upcoming shows per venue.
-  data=[{
-    "city": "San Francisco",
-    "state": "CA",
-    "venues": [{
-      "id": 1,
-      "name": "The Musical Hop",
-      "num_upcoming_shows": 0,
-    }, {
-      "id": 3,
-      "name": "Park Square Live Music & Coffee",
-      "num_upcoming_shows": 1,
-    }]
-  }, {
-    "city": "New York",
-    "state": "NY",
-    "venues": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }]
-  return render_template('pages/venues.html', areas=data);
+  cities = City.query.all();
+  data=[]
+  for city in cities:
+      citydata = {
+        "city": city.city,
+        "state": city.state,
+        "venues": Venue.query.filter_by(cityid=city.id).all()
+      }
+      data.append(citydata)
+
+  return render_template('pages/venues.html', areas=data)
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
