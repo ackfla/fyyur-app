50eaf7772ec9bd2bb1b98df9f57be6240431ddf1
Lucy Weatherley <lucy@geckota.com>
2021-06-08 22:04:59 +0100

    Move models in separate file

--- app.py
+++ app.py
@@ -16,13 +16,13 @@ from flask import (
     abort
 )
 from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
 from flask_migrate import Migrate
 from datetime import date
+from models import db, Venue, Artist, Show
 
 #----------------------------------------------------------------------------#
 # App Config.
@@ -31,64 +31,8 @@ from datetime import date
 app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
-db = SQLAlchemy(app)
+db.init_app(app) # Init db
 migrate = Migrate(app, db)
-
-#----------------------------------------------------------------------------#
-# Models.
-#----------------------------------------------------------------------------#
-
-class City(db.Model):
-    __tablename__ = 'city'
-    id = db.Column(db.Integer, primary_key=True)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    venues = db.relationship('Venue', backref='city', lazy=True)
-    artists = db.relationship('Artist', backref='city', lazy=True)
-    def __repr__(self):
-        return f'<City {self.id} {self.city}>'
-
-class Venue(db.Model):
-    __tablename__ = 'venue'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    address = db.Column(db.String(120), nullable=False)
-    cityid = db.Column(db.Integer, db.ForeignKey('city.id'), nullable=False)
-    phone = db.Column(db.String(120))
-    website = db.Column(db.String(120))
-    facebook_link = db.Column(db.String(120))
-    genres = db.Column(db.String(500))
-    seeking_talent = db.Column(db.Boolean(), default=False)
-    seeking_description = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    shows = db.relationship('Show', backref='venue', lazy=True)
-    def __repr__(self):
-        return f'<Venue {self.id} {self.name}>'
-
-class Artist(db.Model):
-    __tablename__ = 'artist'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    cityid = db.Column(db.Integer, db.ForeignKey('city.id'), nullable=False)
-    phone = db.Column(db.String(120))
-    website = db.Column(db.String(120))
-    facebook_link = db.Column(db.String(120))
-    genres = db.Column(db.String(500))
-    seeking_venue = db.Column(db.Boolean(), default=False)
-    seeking_description = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    shows = db.relationship('Show', backref='artist', lazy=True)
-    def __repr__(self):
-        return f'<Artist {self.id} {self.name}>'
-
-class Show(db.Model):
-    __tablename__ = 'show'
-    id = db.Column(db.Integer, primary_key=True)
-    artistid = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
-    venueid = db.Column(db.Integer, db.ForeignKey('venue.id'), nullable=False)
-    start_time = db.Column(db.DateTime, nullable=False)
-    def __repr__(self):
-        return f'<Show {self.id} {self.start_time}>'
 
 #----------------------------------------------------------------------------#
 # Filters.
---
+++ models.py
@@ -0,0 +1,67 @@
+#----------------------------------------------------------------------------#
+# Imports.
+#----------------------------------------------------------------------------#
+
+from flask_sqlalchemy import SQLAlchemy
+
+#----------------------------------------------------------------------------#
+# App Config.
+#----------------------------------------------------------------------------#
+
+db = SQLAlchemy()
+
+#----------------------------------------------------------------------------#
+# Models.
+#----------------------------------------------------------------------------#
+
+class City(db.Model):
+    __tablename__ = 'city'
+    id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    venues = db.relationship('Venue', backref='city', lazy=True)
+    artists = db.relationship('Artist', backref='city', lazy=True)
+    def __repr__(self):
+        return f'<City {self.id} {self.city}>'
+
+class Venue(db.Model):
+    __tablename__ = 'venue'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    cityid = db.Column(db.Integer, db.ForeignKey('city.id'), nullable=False)
+    phone = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    facebook_link = db.Column(db.String(120))
+    genres = db.Column(db.String(500))
+    seeking_talent = db.Column(db.Boolean(), default=False)
+    seeking_description = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    shows = db.relationship('Show', backref='venue', lazy=True)
+    def __repr__(self):
+        return f'<Venue {self.id} {self.name}>'
+
+class Artist(db.Model):
+    __tablename__ = 'artist'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    cityid = db.Column(db.Integer, db.ForeignKey('city.id'), nullable=False)
+    phone = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    facebook_link = db.Column(db.String(120))
+    genres = db.Column(db.String(500))
+    seeking_venue = db.Column(db.Boolean(), default=False)
+    seeking_description = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    shows = db.relationship('Show', backref='artist', lazy=True)
+    def __repr__(self):
+        return f'<Artist {self.id} {self.name}>'
+
+class Show(db.Model):
+    __tablename__ = 'show'
+    id = db.Column(db.Integer, primary_key=True)
+    artistid = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
+    venueid = db.Column(db.Integer, db.ForeignKey('venue.id'), nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False)
+    def __repr__(self):
+        return f'<Show {self.id} {self.start_time}>'
